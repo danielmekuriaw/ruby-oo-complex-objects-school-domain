231f19b7f735520e564d31af2919e9df66233d35
danielmekuriaw <daniel.mekuriaw16@gmail.com>
2021-06-09 18:15:26 -0500

    complex objects school domain lab solution

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
 # code here!
+
+class School
+
+    attr_accessor :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student_name, grade)
+        if self.roster[grade] == nil
+            self.roster[grade] = []
+            self.roster[grade] << student_name
+        else
+            self.roster[grade] << student_name
+        end
+    end
+
+    def grade(grade)
+        self.roster[grade]
+    end
+
+    def sort
+        self.roster.each{|key, value|
+            value.sort!
+        }
+    end
+
+end
+
+
+
